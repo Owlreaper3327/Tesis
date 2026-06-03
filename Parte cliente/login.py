0ff53448be77717fc33b45df7e85d13a8230e6c4
Owlreaper3327 <owlreaper3327@gmail.com>
2026-06-03 12:03:54 -0400

    La interfaz de inicio de sesión y principal de la aplicación está medio hecha por el momento.

--- Parte cliente/login.py
+++ Parte cliente/login.py
@@ -1,8 +1,37 @@
 """This is the login interface for the application"""
 
-#imports goes here
-import streamlit
+#imports go here
+import streamlit as st
+import requests
+
+
+#functions go here
+async def try_login(username, password):
+    return True   
 
 
 #main code goes here
-streamlit.write("Hellow world!")
+
+st.title("Inicio de sesión", text_alignment="center")
+result = ""
+
+center_c = st.columns(3)[1]
+
+formul = center_c.form(enter_to_submit=True, clear_on_submit=True, key="login"\
+                 , width="content")
+
+formul.write("Este formulario todavía no funciona")
+username = formul.text_input(key="username", label="Nombre de usuario")
+password = formul.text_input(key="password", label="Contraseña", type="password")
+
+submit_btn = formul.form_submit_button(label="Iniciar", type="primary",\
+                          help="Presione el botón tras llenar los campos del formulario con sus credenciales")
+
+if submit_btn:
+    st.write(submit_btn)
+
+    if username and password:
+        result = try_login(username, password)
+        st.success(body="Inicio de sesión probado", title="Mensaje de retroalimentación")
+        if result:
+            st.write("This feature hasn't been implemented")
